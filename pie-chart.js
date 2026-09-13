637aec7dea87d8fc34638e00c593a1231e8775b5
Kassandra <kam.terbeek@gmail.com>
2026-09-13 16:15:02 -0600

    Refactor PieChart to accept position and diameter

--- pie-chart.js
+++ pie-chart.js
@@ -1,38 +1,90 @@
-function PieChart(parent, data) {
-    this.parent = parent;
-    this.data = data;
+function PieChart(x, y, diameter) {
 
-    this.draw = function() {
-        var total = 0;
+  this.x = x;
+  this.y = y;
+  this.diameter = diameter;
+  this.labelSpace = 30;
 
-        for (var i = 0; i < this.data.length; i++) {
-            total += this.data[i].value;
-        }
+  this.get_radians = function(data) {
+    var total = sum(data);
+    var radians = [];
 
-        var lastAngle = 0;
+    for (let i = 0; i < data.length; i++) {
+      radians.push((data[i] / total) * TWO_PI);
+    }
 
-        for (var i = 0; i < this.data.length; i++) {
+    return radians;
+  };
 
-            var angle = map(
-                this.data[i].value,
-                0,
-                total,
-                0,
-                TWO_PI
-            );
+  this.draw = function(data, labels, colours, title) {
 
-            fill(this.data[i].colour);
+    // Test that data is not empty and that each input array is the
+    // same length.
+    if (data.length == 0) {
+      alert('Data has length zero!');
+    } else if (![labels, colours].every((array) => {
+      return array.length == data.length;
+    })) {
+      alert(`Data (length: ${data.length})
+Labels (length: ${labels.length})
+Colours (length: ${colours.length})
+Arrays must be the same length!`);
+    }
 
-            arc(
-                this.parent.width / 2,
-                this.parent.height / 2,
-                this.parent.width,
-                this.parent.height,
-                lastAngle,
-                lastAngle + angle
-            );
+    // https://p5js.org/examples/form-pie-chart.html
 
-            lastAngle += angle;
-        }
-    };
+    var angles = this.get_radians(data);
+    var lastAngle = 0;
+    var colour;
+
+    for (var i = 0; i < data.length; i++) {
+      if (colours) {
+        colour = colours[i];
+      } else {
+        colour = map(i, 0, data.length, 0, 255);
+      }
+
+      fill(colour);
+      stroke(0);
+      strokeWeight(1);
+
+      arc(
+        this.x,
+        this.y,
+        this.diameter,
+        this.diameter,
+        lastAngle,
+        lastAngle + angles[i] + 0.001
+      ); // Hack for 0!
+
+      if (labels) {
+        this.makeLegendItem(labels[i], i, colour);
+      }
+
+      lastAngle += angles[i];
+    }
+
+    if (title) {
+      noStroke();
+      textAlign('center', 'center');
+      textSize(24);
+      text(title, this.x, this.y - this.diameter * 0.6);
+    }
+  };
+
+  this.makeLegendItem = function(label, i, colour) {
+    var x = this.x + 50 + this.diameter / 2;
+    var y = this.y + (this.labelSpace * i) - this.diameter / 3;
+    var boxWidth = this.labelSpace / 2;
+    var boxHeight = this.labelSpace / 2;
+
+    fill(colour);
+    rect(x, y, boxWidth, boxHeight);
+
+    fill('black');
+    noStroke();
+    textAlign('left', 'center');
+    textSize(12);
+    text(label, x + boxWidth + 10, y + boxWidth / 2);
+  };
 }
