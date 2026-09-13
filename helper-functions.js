a7c4077ce248288e6baf688f9ce3a23df61d7d20
Kassandra <kam.terbeek@gmail.com>
2026-09-13 16:09:37 -0600

    Refactor helper functions for consistency

--- helper-functions.js
+++ helper-functions.js
@@ -1,236 +1,163 @@
-// --------------------------------------------------------------------
-// Data processing helper functions.
-// --------------------------------------------------------------------
-
 function sum(data) {
-    var total = 0;
-
-    // Ensure that data contains numbers and not strings.
-    data = stringsToNumbers(data);
-
-    for (let i = 0; i < data.length; i++) {
-        total = total + data[i];
-    }
-
-    return total;
+  var total = 0;
+  data = stringsToNumbers(data);
+  for (let i = 0; i < data.length; i++) {
+    total = total + data[i];
+  }
+  return total;
 }
-
 
 function mean(data) {
-    var total = sum(data);
-
-    return total / data.length;
+  var total = sum(data);
+  return total / data.length;
 }
-
-
-// --------------------------------------------------------------------
-// Summary statistics
-// --------------------------------------------------------------------
-
-function calculateStats(data) {
-
-    if (data.length == 0) {
-        return {
-            min: 0,
-            max: 0,
-            mean: 0
-        };
-    }
-
-    data = stringsToNumbers(data);
-
-    var minimum = data[0];
-    var maximum = data[0];
-
-    for (var i = 1; i < data.length; i++) {
-
-        if (data[i] < minimum) {
-            minimum = data[i];
-        }
-
-        if (data[i] > maximum) {
-            maximum = data[i];
-        }
-    }
-
-    return {
-        min: minimum,
-        max: maximum,
-        mean: mean(data)
-    };
-}
-
-
-// --------------------------------------------------------------------
-// Other data processing helpers
-// --------------------------------------------------------------------
 
 function sliceRowNumbers(row, start = 0, end) {
-
-    var rowData = [];
-
-    if (!end) {
-        // Parse all values until the end of the row.
-        end = row.arr.length;
-    }
-
-    for (i = start; i < end; i++) {
-        rowData.push(row.getNum(i));
-    }
-
-    return rowData;
+  var rowData = [];
+  if (!end) end = row.arr.length;
+  for (i = start; i < end; i++) {
+    rowData.push(row.getNum(i));
+  }
+  return rowData;
 }
-
 
 function stringsToNumbers(array) {
-    return array.map(Number);
+  return array.map(Number);
 }
-
-
-// --------------------------------------------------------------------
-// Plotting helper functions
-// --------------------------------------------------------------------
 
 function drawAxis(layout, colour = 0) {
-
-    stroke(color(colour));
-
-    // x-axis
-    line(
-        layout.leftMargin,
-        layout.bottomMargin,
-        layout.rightMargin,
-        layout.bottomMargin
-    );
-
-    // y-axis
-    line(
-        layout.leftMargin,
-        layout.topMargin,
-        layout.leftMargin,
-        layout.bottomMargin
-    );
+  stroke(color(colour));
+  line(
+    layout.leftMargin,
+    layout.bottomMargin,
+    layout.rightMargin,
+    layout.bottomMargin
+  );
+  line(
+    layout.leftMargin,
+    layout.topMargin,
+    layout.leftMargin,
+    layout.bottomMargin
+  );
 }
-
 
 function drawAxisLabels(xLabel, yLabel, layout) {
+  fill(0);
+  noStroke();
+  textAlign('center', 'center');
 
-    fill(0);
-    noStroke();
+  text(
+    xLabel,
+    (layout.plotWidth() / 2) + layout.leftMargin,
+    layout.bottomMargin + (layout.marginSize * 1.5)
+  );
 
-    textAlign('center', 'center');
+  push();
 
-    // Draw x-axis label.
-    text(
-        xLabel,
-        (layout.plotWidth() / 2) + layout.leftMargin,
-        layout.bottomMargin + (layout.marginSize * 1.5)
-    );
+  translate(
+    layout.leftMargin - (layout.marginSize * 1.5),
+    layout.bottomMargin / 2
+  );
 
-    // Draw y-axis label.
-    push();
+  rotate(-PI / 2);
 
-    translate(
-        layout.leftMargin - (layout.marginSize * 1.5),
-        layout.bottomMargin / 2
-    );
+  text(yLabel, 0, 0);
 
-    rotate(-PI / 2);
-
-    text(
-        yLabel,
-        0,
-        0
-    );
-
-    pop();
+  pop();
 }
-
 
 function drawYAxisTickLabels(
-    min,
-    max,
-    layout,
-    mapFunction,
-    decimalPlaces
+  min,
+  max,
+  layout,
+  mapFunction,
+  decimalPlaces
 ) {
+  var range = max - min;
+  var yTickStep = range / layout.numYTickLabels;
 
-    // Map function must be passed with .bind(this).
-    var range = max - min;
+  fill(0);
+  noStroke();
+  textAlign('right', 'center');
 
-    var yTickStep =
-        range / layout.numYTickLabels;
+  for (i = 0; i <= layout.numYTickLabels; i++) {
+    var value = min + (i * yTickStep);
+    var y = mapFunction(value);
 
-    fill(0);
-    noStroke();
-
-    textAlign('right', 'center');
-
-    // Draw all axis tick labels and grid lines.
-    for (i = 0; i <= layout.numYTickLabels; i++) {
-
-        var value =
-            min + (i * yTickStep);
-
-        var y =
-            mapFunction(value);
-
-        // Add tick label.
-        text(
-            value.toFixed(decimalPlaces),
-            layout.leftMargin - layout.pad,
-            y
-        );
-
-        if (layout.grid) {
-
-            // Add grid line.
-            stroke(200);
-
-            line(
-                layout.leftMargin,
-                y,
-                layout.rightMargin,
-                y
-            );
-        }
-    }
-}
-
-
-function drawXAxisTickLabel(
-    value,
-    layout,
-    mapFunction
-) {
-
-    // Map function must be passed with .bind(this).
-    var x =
-        mapFunction(value);
-
-    fill(0);
-    noStroke();
-
-    textAlign('center', 'center');
-
-    // Add tick label.
     text(
-        value,
-        x,
-        layout.bottomMargin +
-        layout.marginSize / 2
+      value.toFixed(decimalPlaces),
+      layout.leftMargin - layout.pad,
+      y
     );
 
     if (layout.grid) {
-
-        // Add grid line.
-        stroke(220);
-
-        line(
-            x,
-            layout.topMargin,
-            x,
-            layout.bottomMargin
-        );
+      stroke(200);
+      line(
+        layout.leftMargin,
+        y,
+        layout.rightMargin,
+        y
+      );
     }
+  }
 }
+
+function drawXAxisTickLabel(value, layout, mapFunction) {
+  var x = mapFunction(value);
+
+  fill(0);
+  noStroke();
+  textAlign('center', 'center');
+
+  text(
+    value,
+    x,
+    layout.bottomMargin + layout.marginSize / 2
+  );
+
+  if (layout.grid) {
+    stroke(220);
+    line(
+      x,
+      layout.topMargin,
+      x,
+      layout.bottomMargin
+    );
+  }
+}
+
+
+/* Start - own code */
+
+function calculateStats(data) {
+  if (data.length == 0) {
+    return {
+      min: 0,
+      max: 0,
+      mean: 0
+    };
+  }
+
+  data = stringsToNumbers(data);
+
+  var minimum = data[0];
+  var maximum = data[0];
+
+  for (var i = 1; i < data.length; i++) {
+    if (data[i] < minimum) {
+      minimum = data[i];
+    }
+
+    if (data[i] > maximum) {
+      maximum = data[i];
+    }
+  }
+
+  return {
+    min: minimum,
+    max: maximum,
+    mean: mean(data)
+  };
+}
+
+/* End - own code */
