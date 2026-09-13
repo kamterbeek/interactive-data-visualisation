92e9920f708a4416f8edc1f35675627ec3594bb8
Kassandra <kam.terbeek@gmail.com>
2026-09-13 16:16:38 -0600

    Refactor setup function to reduce redundancy

--- sketch.js
+++ sketch.js
@@ -1,56 +1,43 @@
 // Global variable to store the gallery object.
 var gallery;
 
-
 function setup() {
+  // Create a canvas to fill the content div from index.html.
+  var c = createCanvas(1024, 576);
+  c.parent('app');
 
-    // Create the canvas.
-    var c = createCanvas(1024, 576);
-    c.parent('app');
+  // Create a new gallery object.
+  gallery = new Gallery();
 
-    // Create the Gallery object.
-    gallery = new Gallery();
+  // Add the visualisation objects here.
+  gallery.addVisual(new TechDiversityRace());
+  gallery.addVisual(new TechDiversityGender());
+  gallery.addVisual(new PayGapByJob2017());
+  gallery.addVisual(new PayGapTimeSeries());
+  gallery.addVisual(new ClimateChange());
 
-    // Add all five visualisations to the gallery.
-    gallery.addVisual(new TechDiversityRace());
-    gallery.addVisual(new TechDiversityGender());
-    gallery.addVisual(new PayGapByJob2017());
-    gallery.addVisual(new PayGapTimeSeries());
-    gallery.addVisual(new ClimateChange());
+  /* Start - own code */
 
+  // Connect the visualisation dropdown in index.html to the Gallery.
+  var graphSelect = document.getElementById('graphSelect');
 
-    // Connect the visualisation dropdown
-    // in index.html to the Gallery.
-    var graphSelect = document.getElementById('graphSelect');
+  if (graphSelect) {
+    graphSelect.addEventListener('change', function() {
+      var selectedGraph = this.value;
 
-    if (graphSelect) {
+      if (selectedGraph != "") {
+        gallery.selectVisual(selectedGraph);
+      }
+    });
+  }
 
-        graphSelect.addEventListener('change', function() {
-
-            var selectedGraph = this.value;
-
-            // Only change visualisation if the user
-            // has selected an option.
-            if (selectedGraph != "") {
-
-                gallery.selectVisual(selectedGraph);
-            }
-        });
-    }
+  /* End - own code */
 }
 
-
 function draw() {
+  background(255);
 
-    // Clear the canvas.
-    background(255);
-
-    // Draw the currently selected visualisation.
-    if (
-        gallery != null &&
-        gallery.selectedVisual != null
-    ) {
-
-        gallery.selectedVisual.draw();
-    }
+  if (gallery.selectedVisual != null) {
+    gallery.selectedVisual.draw();
+  }
 }
