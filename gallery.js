5a2431a004e111cc3893f85f0a06a6610097c6b0
Kassandra <kam.terbeek@gmail.com>
2026-09-13 16:08:36 -0600

    Refactor Gallery methods for better validation

--- gallery.js
+++ gallery.js
@@ -1,65 +1,85 @@
 function Gallery() {
-    this.visuals = [];
-    this.selectedVisual = null;
 
-    this.addVisual = function(vis) {
-        this.visuals.push(vis);
+  this.visuals = [];
+  this.selectedVisual = null;
+  var self = this;
 
-        // Add the visualisation to the original gallery menu.
-        var menuItem = createElement("li", vis.name);
-        menuItem.parent("visuals-menu");
+  this.addVisual = function(vis) {
+    if (!vis.hasOwnProperty('id')
+        && !vis.hasOwnProperty('name')) {
+      alert('Make sure your visualisation has an id and name!');
+    }
 
-        menuItem.mouseClicked(function() {
-            gallery.selectVisual(vis.id);
-        });
+    if (this.findVisIndex(vis.id) != null) {
+      alert(`Vis '${vis.name}' has a duplicate id: '${vis.id}'`);
+    }
 
-        // Load the data for the visualisation.
-        vis.preload();
-    };
+    this.visuals.push(vis);
 
-    this.selectVisual = function(visId) {
-        // Destroy the currently selected visualisation.
-        if (this.selectedVisual != null &&
-            this.selectedVisual.destroy != undefined) {
+    var menuItem = createElement('li', vis.name);
+    menuItem.addClass('menu-item');
+    menuItem.id(vis.id);
 
-            this.selectedVisual.destroy();
-        }
+    menuItem.mouseOver(function(e) {
+      var el = select('#' + e.srcElement.id);
+      el.addClass("hover");
+    });
 
-        // Find the requested visualisation.
-        var index = this.findVisIndex(visId);
+    menuItem.mouseOut(function(e) {
+      var el = select('#' + e.srcElement.id);
+      el.removeClass("hover");
+    });
 
-        if (index != -1) {
-            this.selectedVisual = this.visuals[index];
+    menuItem.mouseClicked(function(e) {
+      var menuItems = selectAll('.menu-item');
+      for (var i = 0; i < menuItems.length; i++) {
+        menuItems[i].removeClass('selected');
+      }
 
-            // Set up the selected visualisation.
-            if (this.selectedVisual.setup != undefined) {
-                this.selectedVisual.setup();
-            }
-        }
-    };
+      var el = select('#' + e.srcElement.id);
+      el.addClass('selected');
 
-    /*
-    NEW: Find a visualisation using its ID. This is used by the new visualisation dropdown in index.html.
-     */
-    this.getVisualById = function(visId) {
-        for (var i = 0; i < this.visuals.length; i++) {
+      self.selectVisual(e.srcElement.id);
+    });
 
-            if (this.visuals[i].id == visId) {
-                return this.visuals[i];
-            }
-        }
+    var visMenu = select('#visuals-menu');
+    visMenu.child(menuItem);
 
-        return null;
-    };
+    if (vis.hasOwnProperty('preload')) {
+      vis.preload();
+    }
+  };
 
-    this.findVisIndex = function(visId) {
-        for (var i = 0; i < this.visuals.length; i++) {
 
-            if (this.visuals[i].id == visId) {
-                return i;
-            }
-        }
+  this.findVisIndex = function(visId) {
+    for (var i = 0; i < this.visuals.length; i++) {
+      if (this.visuals[i].id == visId) {
+        return i;
+      }
+    }
 
-        return -1;
-    };
+    return null;
+  };
+
+
+  this.selectVisual = function(visId) {
+
+    var visIndex = this.findVisIndex(visId);
+
+    if (visIndex != null) {
+
+      if (this.selectedVisual != null
+          && this.selectedVisual.hasOwnProperty('destroy')) {
+        this.selectedVisual.destroy();
+      }
+
+      this.selectedVisual = this.visuals[visIndex];
+
+      if (this.selectedVisual.hasOwnProperty('setup')) {
+        this.selectedVisual.setup();
+      }
+
+      loop();
+    }
+  };
 }
